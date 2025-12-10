cede0bc8435408e3839959191832c438f50887d4
Agon Ajrulla <agon.ajrulla@outlook.com>
2025-12-10 02:01:43 +0100

    Fix database fallback for Koyeb

--- blog/settings.py
+++ blog/settings.py
@@ -110,22 +110,18 @@ WSGI_APPLICATION = 'blog.wsgi.application'
 # }
 
 DATABASES = {}
-if os.getenv("DATABASE_URL"):
-    DATABASES = {
-        'default': dj_database_url.config(
-            default=os.getenv("DATABASE_URL"),
-            conn_max_age=600,
-            ssl_require=True,
-        )
-    }
-else:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': BASE_DIR / "db.sqlite3",
-        }
-    }
 
+if os.getenv("DATABASE_URL"):
+    DATABASES['default'] = dj_database_url.config(
+        default=os.getenv("DATABASE_URL"),
+        conn_max_age=600,
+        ssl_require=True,
+    )
+else:
+    DATABASES['default'] = {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / "db.sqlite3",
+    }
 
 
 
