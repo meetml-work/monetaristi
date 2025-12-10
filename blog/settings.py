3694879a211c2cf2a0b98b056f579e4c0b5eaa44
Agon Ajrulla <agon.ajrulla@outlook.com>
2025-12-10 01:58:01 +0100

    environment stuff

--- blog/settings.py
+++ blog/settings.py
@@ -127,35 +127,6 @@ else:
 
 
 
-# ENV = os.getenv('ENV')
-
-# if ENV == "production":
-#     DATABASES = {
-#         'default': dj_database_url.config(
-#             default=os.getenv("DATABASE_URL"),
-#             conn_max_age=600,
-#             ssl_require=True,
-#         )
-#     }
-# else:
-#     # Local development uses SQLite
-#     DATABASES = {
-#         'default': {
-#             'ENGINE': 'django.db.backends.sqlite3',
-#             'NAME': BASE_DIR / "db.sqlite3",
-#         }
-#     }
-
-
-# DATABASES = {
-#     'default': dj_database_url.config(
-#         default=os.getenv("DATABASE_URL"),
-#         conn_max_age=600,
-#         ssl_require=True,
-#     )
-# }
-
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
